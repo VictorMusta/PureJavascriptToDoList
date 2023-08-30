836b366ca970881f58b4dace77cca6c464bae12c
Victor Grabowski <victor.grabowski@delfox.net>
2023-08-30 14:02:34 +0200

    todolist unfinished in pure javascript (missing copy and copy by reference functions)

--- frontend/scripts/functions.js
+++ frontend/scripts/functions.js
@@ -30,33 +30,39 @@ class Task {
     }
 }
 //déclaration des variables qui serviront plus tard à stocker nos tâches.
-tasksObjects1 = []
-console.log(tasksObjects1);
+let taskListOfObject1 = new Array()
+console.log(taskListOfObject1);
 
-tasksObjects2 = []
+let taskListOfObject2 = new Array()
 
 function myFunction() {
     document.getElementById("addButton").disabled = !document.getElementById("addButton").disabled
 }
 function newTask() {
-    if (tasksObjects1.length == 6) {
+    if (taskListOfObject1.length == 6) {
         alert("6 tasks is the limit!")
+        return
     }
     let taskTitle = document.getElementById("taskTitleInput").value
     if (
-        taskTitle == ""
+        taskTitle === ""
     ) {
         alert("Please enter a valid title in the text field.")
+        return
     }
+    document.getElementById("taskTitleInput").value = ""
 
-    if (tasksObjects1.length < 6) {
-        if (tasksObjects1.length == 5) {
+    if (taskListOfObject1.length < 6) {
+        if (taskListOfObject1.length == 5) {
             document.getElementById("addButton").disabled = true
         }
-        let newTask = new Task(taskTitle, Date.now())
-        tasksObjects1.push(newTask)
+        let myTask = new Task(taskTitle, Date.now())
+        taskListOfObject1.push(myTask)
         //création d'un div pour contenir tout le contenu d'une carte "tâche"
         let taskElement = document.createElement('div')
+        taskElement.className = "taskCard"
+        taskElement.id = myTask.getCreationDate()
+        taskElement.setAttribute('onclick', `setSelectedTask(${myTask.getCreationDate()})`)
 
         //création d'un élément affichant le titre de notre tâche
         let titleField = document.createElement("h3")
@@ -68,7 +74,11 @@ function newTask() {
         colorField.setAttribute('type', "text")
         colorField.setAttribute('class', "colorField")
         colorField.setAttribute('placeholder', "type a color to change")
-        colorField.setAttribute('onchange', colorField.setAttribute("background-color", "orange"))
+        colorField.setAttribute('oninput',"this.parentElement.setAttribute('style', `background-color: ${this.value}`)"
+        // console.log(this.parentElement.id)"
+        // // "taskElement.setAttribute('style', `background-color: ${colorField.textContent}`)"
+
+        )
 
         //création d'un élément pour switcher le status de la tâche.
         let resolveButton = document.createElement("button")
@@ -78,10 +88,10 @@ function newTask() {
         taskElement.appendChild(titleField)
         taskElement.appendChild(colorField)
 
-        taskElement.className = "taskCard"
-        taskElement.id = newTask.getCreationDate()
-        taskElement.setAttribute('onclick', `setSelectedTask(newTask.getCreationDate())`)
+       
         document.getElementById('taskListDiv1').appendChild(taskElement)
+        document.getElementById('taskListDiv1').appendChild(taskElement)
+
     }
     else {
         alert("6 tasks is the limit! Delete tasks or move them elsewhere.")
@@ -92,13 +102,13 @@ function deleteAllTasks() {
 
     let tasks = document.querySelectorAll(".taskCard")
     document.getElementById("addButton").disabled = false;
-
+    taskListOfObject1 = []
+    taskListOfObject2 = []
     tasks.forEach(task => task.remove())
 }
 
 function setSelectedTask(idTask) {
     let selectedTask = document.getElementById(idTask)
-    console.log(selectedTask.id)
     if (selectedTask.className.search("selectedTask") != -1) {
         selectedTask.className = "taskCard"
     }
@@ -106,11 +116,41 @@ function setSelectedTask(idTask) {
         selectedTask.className += " selectedTask"
     }
 }
-function deleteSelectedTask() {
-    let selectedTasks = document.querySelectorAll(".selectedTask")
-    if (selectedTasks.length !== 0) {
-        document.getElementById("addButton").disabled = false;
-        selectedTasks.forEach(task => task.remove())
 
+function deleteSelectedTask() {
+    let selectedTasksElements = document.querySelectorAll(".selectedTask")
+    if (selectedTasksElements.length !== 0) {
+        document.getElementById("addButton").disabled = false;
+        selectedTasksElements.forEach(selectedTask => {
+            let objectToDelete = taskListOfObject1.find((myObject) => myObject.getCreationDate() == selectedTask.id)
+            taskListOfObject1 = taskListOfObject1.filter( (i) => {return i !== objectToDelete}
+            )
+            selectedTask.remove()
+        })
+    }
+}
+
+function moveSelectedTask(shouldErase){
+    let selectedTasksElements = document.querySelectorAll(".selectedTask")
+    if (selectedTasksElements.length !== 0) {
+        document.getElementById("addButton").disabled = false;
+        selectedTasksElements.forEach(selectedTaskElement => {
+            if(selectedTaskElement.parentElement.id === "taskListDiv2"){
+                let taskObjectToMove = taskListOfObject2.find((myObject) => myObject.getCreationDate() == selectedTaskElement.id)
+                taskListOfObject1.push(taskObjectToMove)
+                document.getElementById('taskListDiv1').appendChild(selectedTaskElement)
+                if(shouldErase === true){
+                    taskListOfObject2 = taskListOfObject2.filter( (i) => {return i !== taskObjectToMove})
+                }
+            }
+            else{
+                let taskObjectToMove = taskListOfObject1.find((myObject) => myObject.getCreationDate() == selectedTaskElement.id)
+                taskListOfObject2.push(taskObjectToMove)
+                document.getElementById('taskListDiv2').appendChild(selectedTaskElement)
+                if(shouldErase === true){
+                    taskListOfObject1 = taskListOfObject1.filter( (i) => {return i !== taskObjectToMove})
+                }
+            }
+        })
     }
 }
